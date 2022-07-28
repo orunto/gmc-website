04825de4e03f71503e88da1a49c2941ce9a55d41
Orunto Eniola <oruntoeniola@aol.com>
2022-07-28 15:25:27 +0100

    For Men Section responsive

--- src/containers/NavBar.jsx
+++ src/containers/NavBar.jsx
@@ -1,4 +1,7 @@
 import Logo from '../images/Logo.png';
+import LogoMobile from '../images/LogoMobile.png';
+import { ReactComponent as MenuIco} from '../images/MenuIco.svg';
+import { ReactComponent as ProductDropdown} from '../images/ProductDropdown.svg';
 import { Button } from '../components';
 
 import '../css/NavBar.css';
@@ -7,8 +10,12 @@ const NavBar = () => {
     return(
         <nav>
             <img src={Logo} alt="Logo"/>
+            <div className='MobileNavBar'>
+                <img src={LogoMobile} alt="Logo" id="mobile"/>
+                <MenuIco id="menu"/>
+            </div>
             <ul>
-                <li>Products</li>
+                <li>Products <ProductDropdown/></li>
                 <li>Company</li>
                 <li>Support</li>
                 <li>Reads</li>
