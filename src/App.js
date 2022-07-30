23593bb04af59e672c2c78f9f70200d44947dfe6
Orunto Eniola <oruntoeniola@aol.com>
2022-07-30 01:16:22 +0100

    Reads page creation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Home } from './pages';
+import { Home, Reads } from './pages';
 import './App.css';
 
 function App() {
@@ -7,6 +7,7 @@ function App() {
     <Router> 
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/reads" element={<Reads/>} />
       </Routes>
     </Router>
   );
---
+++ src/pages/Reads.jsx
@@ -0,0 +1,14 @@
+import { NavBar, Footer } from '../containers';
+
+import '../css/Reads.css';
+
+const Reads = () => {
+    return(
+        <div>
+            <NavBar/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default Reads;
