adbdb005ab39be54636d7982086295f0b9570cd9
Orunto Eniola <oruntoeniola@aol.com>
2022-08-04 16:27:58 +0100

    NavBar corrections

--- src/containers/NavBar.jsx
+++ src/containers/NavBar.jsx
@@ -1,4 +1,4 @@
-import Logo from '../images/Logo.png';
+// import Logo from 'https://cdn.jsdelivr.net/gh/orunto/mycdn/gcrib/images/Logo2.png';
 import LogoMobile from '../images/LogoMobile.png';
 import { ReactComponent as MenuIco} from '../images/MenuIco.svg';
 import { ReactComponent as CancelIco} from '../images/MenuIco.svg';
@@ -51,7 +51,7 @@ function Fold () {
 const NavBar = () => {
     return(
         <nav id="nav">
-            <img src={Logo} alt="Logo"/>
+            <img src='https://cdn.jsdelivr.net/gh/orunto/mycdn/gcrib/images/Logo2.png' alt="Logo"/>
             <div className='MobileNavBar'>
                 <img src={LogoMobile} alt="Logo" id="mobile"/>
                 <MenuIco id="menu" onClick={Drop}/>
