c6ddb415bcf4aef0a0610d516144e45249b817a8
Orunto Eniola <oruntoeniola@aol.com>
2022-08-07 13:44:37 +0100

    7 Formal Accessories To Make You Appear Majestic

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ export { default as Home } from './Home';
 export { default as Reads } from './Reads';
 export { default as FiveOutfitMistakes } from './reads/FiveOutfitMistakes';
 export { default as NineAccessories } from './reads/NineAccessories';
+export { default as AppearMajestic } from './reads/AppearMajestic';
 
 export { default as PrivacyPolicy } from './PrivacyPolicy';
 export { default as TermsOfService } from './TermsOfService';
