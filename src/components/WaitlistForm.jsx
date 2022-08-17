ed8f26c5cc612eae20b3f028c765dc8b09f8c735
Orunto Eniola <oruntoeniola@aol.com>
2022-08-17 10:40:27 +0100

    Basic waitlist functionality

--- src/components/WaitlistForm.jsx
+++ src/components/WaitlistForm.jsx
@@ -1,9 +1,22 @@
 const WaitlistForm = () => {
     return(
         <div className='WaitlistForm'>
-            <form>
-                <input id="tel" type='tel' placeholder="000-0000-0000"/>
-                <input id="submitbtn" type='submit' value="Join the Waitlist"/>
+            <form action='https://submit-form.com/KSHU6qfv'>
+                <input
+                    type="hidden"
+                    name="_feedback.success.title"
+                    value="Welcome to the Waitlist!"
+                    id="waitlisth2"
+                />
+                <input
+                    type="hidden"
+                    name="_feedback.success.message"
+                    value="You’ll be the first to know when it’s ready!"
+                    id="waitlisth2"
+                />
+                <input type="hidden" name="_feedback.dark" value="true" />
+                <input id="tel" name="Phone Number" type='tel' placeholder="000-0000-0000"/>
+                <input id="submitbtn" type='submit' name='submit' value="Join the Waitlist"/>
             </form>
         </div>
     )
