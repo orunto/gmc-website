ee6cc65d01058fff306595c97725ac25db031c22
Orunto Eniola <oruntoeniola@aol.com>
2022-09-23 11:43:14 +0100

    Support page responsive

--- src/pages/Support.jsx
+++ src/pages/Support.jsx
@@ -15,7 +15,8 @@ const Support = () => {
             <div className='StandardHeader'>
                 <h1>We’re Here to Support You</h1>
                 <h4>So get in touch</h4>
-                <form className='SupportFieldContainer'action='https://submit-form.com/ZcTzSW3f'><input
+                <form className='SupportFieldContainer' action='https://submit-form.com/ZcTzSW3f'>
+                <input
                     type="hidden"
                     name="_feedback.success.title"
                     value="Your message has been recieved!"
@@ -27,16 +28,16 @@ const Support = () => {
                     value="We'll follow up with you shortly"
                     id="waitlisth2"
                 />
-                    <input className='RegularField' type='text' placeholder='Name'/>
-                    <input className='RegularField' type='email' placeholder='Email Address'/>
-                    <input className='RegularField' type='tel' placeholder='Phone Number'/>
-                    <select className='RegularField'>
+                    <input required name='Name' className='RegularField' type='text' placeholder='Name'/>
+                    <input requiredname='Email' className='RegularField' type='email' placeholder='Email Address'/>
+                    <input requiredname='Phone Number' className='RegularField' type='tel' placeholder='Phone Number'/>
+                    <select required name='Topic' className='RegularField'>
                         <option value="" disabled selected>Select a topic</option>
                         <option value="starter packs">Starter Packs</option>
                         <option value="our app">Our app</option>
                         <option value="skin care">Skin Care</option>
                     </select>
-                    <input className='MessageField' type='text' placeholder='Message'/>
+                    <textarea required name='Message' className='MessageField' type='text' placeholder='Message'></textarea>
                     <Button type='submit'>Send</Button>
                 </form>
             </div>
