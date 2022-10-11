3156c5f478d638cf81774df7079bf3b6b84be3de
Orunto Eniola <oruntoeniola@aol.com>
2022-10-11 15:58:32 +0100

    Spotlight read update

--- src/containers/SpotlightRead.jsx
+++ src/containers/SpotlightRead.jsx
@@ -8,8 +8,8 @@ const SpotlightRead = () => {
             <div className='SpotlightReadInfo'>
                 <h3>Spotlight Read</h3>
                  
-                <h2>Skincare Products Every Man Should Have</h2>
-                <Button><a href='/skincareproducts'>Read More</a></Button>
+                <h2>10 Clothing Items to Elevate Your Wardrobe for Work</h2>
+                <Button><a href='/clothingitemstoelevateyourwardrobe'>Read More</a></Button>
             </div>
             <PreviousSpotlight/>
             <RelatedReads/>
