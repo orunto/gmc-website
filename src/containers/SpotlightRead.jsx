2dc6e0a08754bc7da26b3e742b4c183ba20a36be
Orunto Eniola <oruntoeniola@aol.com>
2022-10-21 12:30:30 +0100

    Spotlight read update

--- src/containers/SpotlightRead.jsx
+++ src/containers/SpotlightRead.jsx
@@ -8,8 +8,8 @@ const SpotlightRead = () => {
             <div className='SpotlightReadInfo'>
                 <h3>Spotlight Read</h3>
                  
-                <h2>How to Add Colour to your Outfit like a Gentleman</h2>
-                <Button><a href='/colourtoyouroutfit'>Read More</a></Button>
+                <h2>Guide To Thrift Shopping In Nigeria (5 Tips For Thrifting)</h2>
+                <Button><a href='/tipsforthrifting'>Read More</a></Button>
             </div>
             <PreviousSpotlight/>
             <RelatedReads/>
