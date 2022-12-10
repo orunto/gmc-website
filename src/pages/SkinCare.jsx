bf71a43e0af35656dc910c315628e827ce7e9465
Orunto Eniola <oruntoeniola@aol.com>
2022-12-10 20:49:57 +0100

    fix: Skin care link

--- src/pages/SkinCare.jsx
+++ src/pages/SkinCare.jsx
@@ -15,7 +15,7 @@ const SkinCare = () => {
             <div className='StandardHeader'>
                 <h1>Free Skincare Consultations for Men</h1>
                 <h4>Talk to an expert on improving your skin’s health and get recommendations for routines and products within your budget.</h4>
-                <a href="https://wa.me/2348137290744"><Button>Start Free Consultation</Button></a>
+                <a href="https://wa.me/2349072419839?text=Hello%2C%20I%20would%20like%20the%20Gentlemen%27s%20Crib%20Free%20men%27s%20Skincare%20consultation%20and%20recommendation"><Button>Start Free Consultation</Button></a>
             </div>
         </div>
     )
