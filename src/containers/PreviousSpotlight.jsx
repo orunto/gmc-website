ddab2bef933d8f350c5725bc6f2be3962ae9780b
Orunto Eniola <oruntoeniola@aol.com>
2022-12-10 19:48:59 +0100

    feat: Previous Spotlight Update

--- src/containers/PreviousSpotlight.jsx
+++ src/containers/PreviousSpotlight.jsx
@@ -2,11 +2,11 @@ import '../css/Reads.css';
 
 const PreviousSpotlight = () => {
     return(
-        <a href='/colourtoyouroutfit'>
+        <a href='/pickingdateoutfits'>
             <div className='PreviousSpotlight'>
                 <h3>Previous Spotlight</h3>
                 <div className='PreviousSpotlightImage'></div>
-                <h4>How to Add Colour to your Outfit like a Gentleman</h4>
+                <h4>5 Tips to Consider When Picking Date Outfits for Men</h4>
             </div>
         </a>
     )
